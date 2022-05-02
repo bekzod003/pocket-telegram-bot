a781d253761c3e7ba49ec0e1fb4c31e48ee611cc
bekzod003 <begiobm@gmail.com>
2022-05-02 22:21:07 +0500

    telegram bot struct constructor and start methods

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/bekzod003/pocket-telegram-bot/pkg/telegram"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -14,21 +15,7 @@ func main() {
 
 	bot.Debug = true
 
-	log.Printf("Authorized on account @%s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
-		}
+	if err := telegram.NewBot(bot).Start(); err != nil {
+		log.Fatal("Error while starting bot: ", err)
 	}
 }
