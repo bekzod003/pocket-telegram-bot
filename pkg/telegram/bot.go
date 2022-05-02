c18b06bc3dd8c7b9864ee0c78b71a3ca4d1e8bcf
bekzod003 <begiobm@gmail.com>
2022-05-02 22:30:47 +0500

    handle signle update method

--- pkg/telegram/bot.go
+++ pkg/telegram/bot.go
@@ -29,15 +29,21 @@ func (b *Bot) Start() error {
 // Private method to handle updates from telegram bot
 func (b *Bot) handleUpdates(updates tgbotapi.UpdatesChannel) {
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			b.bot.Send(msg)
+		if update.Message == nil { // If we got no message
+			continue
 		}
+		b.handleUpdate(update)
 	}
+}
+
+// Handle update from telegram bot
+func (b *Bot) handleUpdate(update tgbotapi.Update) {
+	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+	msg.ReplyToMessageID = update.Message.MessageID
+
+	b.bot.Send(msg)
 }
 
 // Initialize updates channel
